56b3408eac637f3f31dbcd4ceacec1e3d9b9dc03
Godfrey Meyer <gm3beats@gmail.com>
2022-05-27 17:27:11 -0600

    fixed floor

--- src/game.ts
+++ src/game.ts
@@ -33,6 +33,98 @@ const transform11 = new Transform({
 })
 triggerArea.addComponentOrReplace(transform11)
 
+const entity = new Entity('entity')
+engine.addEntity(entity)
+entity.setParent(_scene)
+const gltfShape = new GLTFShape("c9b17021-765c-4d9a-9966-ce93a9c323d1/FloorBaseGrass_01/FloorBaseGrass_01.glb")
+gltfShape.withCollisions = true
+gltfShape.isPointerBlocker = true
+gltfShape.visible = true
+entity.addComponentOrReplace(gltfShape)
+const transform2 = new Transform({
+  position: new Vector3(8, 0, 8),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity.addComponentOrReplace(transform2)
+
+const entity2 = new Entity('entity2')
+engine.addEntity(entity2)
+entity2.setParent(_scene)
+entity2.addComponentOrReplace(gltfShape)
+const transform3 = new Transform({
+  position: new Vector3(24, 0, 8),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity2.addComponentOrReplace(transform3)
+
+const entity3 = new Entity('entity3')
+engine.addEntity(entity3)
+entity3.setParent(_scene)
+entity3.addComponentOrReplace(gltfShape)
+const transform4 = new Transform({
+  position: new Vector3(8, 0, -8),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity3.addComponentOrReplace(transform4)
+
+const entity4 = new Entity('entity4')
+engine.addEntity(entity4)
+entity4.setParent(_scene)
+entity4.addComponentOrReplace(gltfShape)
+const transform5 = new Transform({
+  position: new Vector3(24, 0, -8),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity4.addComponentOrReplace(transform5) 
+
+const entity5 = new Entity('entity5')
+engine.addEntity(entity5)
+entity5.setParent(_scene)
+entity5.addComponentOrReplace(gltfShape)
+const transform6 = new Transform({
+  position: new Vector3(8, 0, -24),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity5.addComponentOrReplace(transform6)
+
+const entity6 = new Entity('entity6')
+engine.addEntity(entity6)
+entity6.setParent(_scene)
+entity6.addComponentOrReplace(gltfShape)
+const transform7 = new Transform({
+  position: new Vector3(24, 0, -24),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity6.addComponentOrReplace(transform7) 
+
+const entity7 = new Entity('entity7')
+engine.addEntity(entity7)
+entity7.setParent(_scene)
+entity7.addComponentOrReplace(gltfShape)
+const transform8 = new Transform({
+  position: new Vector3(24, 0, -40),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity7.addComponentOrReplace(transform8)
+
+const entity8 = new Entity('entity8')
+engine.addEntity(entity8)
+entity8.setParent(_scene)
+entity8.addComponentOrReplace(gltfShape)
+const transform9 = new Transform({
+  position: new Vector3(8, 0, -40),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity8.addComponentOrReplace(transform9)  
+
 const messageBubble = new Entity('messageBubble')
 engine.addEntity(messageBubble)
 messageBubble.setParent(_scene)
